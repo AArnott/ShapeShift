e6f585b3c3a61f4b539264beec12b4aea4c438ed
Andrew Arnott <andrew.arnott@microsoft.com>
2025-12-31 10:26:04 -0700

    Skip failing test

--- test/ShapeShift.Taml.Tests/TamlSerializerTests.cs
+++ test/ShapeShift.Taml.Tests/TamlSerializerTests.cs
@@ -52,7 +52,7 @@ public partial class TamlSerializerTests : TestBase
 		await this.AssertRoundtripAsync(person);
 	}
 
-	[Test]
+	[Test, Skip("https://github.com/csharpfritz/Taml/issues/34")]
 	public async Task ListOfRecords()
 	{
 		Family family = new()
